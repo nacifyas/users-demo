ddad675d6929ba5c1710d52990ee53856707a503
nacifyas <nacifyas@gmail.com>
2022-07-17 23:06:21 +0200

    Removed unecessary redis conexion

--- src/config/redis_conf.py
+++ src/config/redis_conf.py
@@ -1,20 +1,10 @@
 from aredis_om import get_redis_connection
 
-ENCODING = "utf-8"
-REDIS_URL = f"redis://atomflare.af:6388"
-
-redis = get_redis_connection(
-    url=REDIS_URL,
-    encoding=ENCODING,
-    decode_responses=True,
-    db=2
-)
-
 STREAM_URL = f"redis://atomflare.af:6388"
 
 redis_stream = get_redis_connection(
     url=STREAM_URL,
     encoding=ENCODING,
     decode_responses=True,
-    db=0
+    db=1
 )
